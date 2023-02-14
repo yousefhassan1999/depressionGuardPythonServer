4b845c02d0e146d8692487e220c284b9eb1b5912
yousefhassan1999 <yousefhassan121999@gmail.com>
2023-02-14 13:27:18 +0200

    done

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+import snscrape.modules.twitter as sntwitter
+
+filename = 'Resources/pipeline.pkl'
+
+with open(filename, 'rb') as f:
+  pipeline = pickle.load(f)
+  
+app = Flask(__name__)
+api = Api(app)
+
+parser1 = reqparse.RequestParser()
+parser1.add_argument('Post',type=str,help="Post is required",required=True)
+
+parser2 = reqparse.RequestParser()
+parser2.add_argument('Username',type=str,help="Username is required",required=True)
+parser2.add_argument('From',type=str,help="From is required",required=True)
+parser2.add_argument('To',type=str,help="To is required",required=True)
+
+class ModelPrediction(Resource):
+    def get(self):
+        args = parser1.parse_args()
+        PostToPredict = args['Post']
+        return pipeline(PostToPredict)
+    
+    
+class GetUserTwittes(Resource):
+    def get(self):
+        args2 = parser2.parse_args()
+        Username = args2['Username']
+        From = args2['From']
+        To = args2['To']
+        tweets = []
+        for tweet in sntwitter.TwitterSearchScraper(f'(from:{Username}) until:{To} since:{From}').get_items():
+            tweets.append([tweet.id,tweet.rawContent])
+            if len(tweets) > 5:
+                break;
+        return tweets
+    
+    
+    
+api.add_resource(ModelPrediction, '/predict')
+api.add_resource(GetUserTwittes, '/GetUserTwittes')
+
+if __name__ == '__main__':
+    app.run(debug=True)
